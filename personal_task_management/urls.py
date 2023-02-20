acb4e625a588cce60071fa82660560aca1f81a55
Claire <claire.cy.t@gmail.com>
2023-02-20 03:54:53 -0500

    enable logout and add home page

--- personal_task_management/urls.py
+++ personal_task_management/urls.py
@@ -20,5 +20,7 @@ from todo import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/", views.signupuser, name="signupuser"),
+    path("logout/", views.logoutuser, name="logoutuser"),
     path("current/", views.currenttodos, name="currenttodos"),
+    path("", views.home, name="home"),
 ]
